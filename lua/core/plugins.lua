ee9517d828518b97928778f2c7fdf2011dbd2be9
KotDimos <KotDimos@gmail.com>
2024-08-24 12:42:22 +0300

    chore: delete plugin

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -52,18 +52,6 @@ require("lazy").setup({
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
     {
-      "max397574/better-escape.nvim",
-      config = function()
-        require("better_escape").setup({
-            mapping = {"jj"},
-        	timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
-    	    clear_empty_lines = false, -- clear line after escaping if there is only whitespace
-    	    keys = "<Esc>",
-        })
-      end
-    },
-
-    {
       'numToStr/Comment.nvim',
       opts = {
           -- add any options here
