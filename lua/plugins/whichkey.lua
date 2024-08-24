067a2ba875e890fca035824cefde96c6fdc88206
KotDimos <KotDimos@gmail.com>
2024-08-24 14:27:42 +0300

    refactor: update whichkey for new version

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,43 +1,6 @@
-local wk = require("which-key")
-
-wk.register({
-	f = {
-		name = "Find",
-		f = {"Find File"},
-		t = {"Find Text"},
-		b = {"Find Buffer"},
-		h = {"Find Help"},
-	},
-
-	g = {
-		name = "Git",
-		b = "Open Branches",
-		c = "Open Commits",
-		s = "Open Status",
-	},
-
-    e = {"Open Diagnostic Window"},
-
-    l = {
-        name = "LSP",
-        D = "Declaration",
-        d = "Definition",
-        k = "Hover",
-    },
-
-    t = {
-        name = "NvimTree",
-        t = "Tree Toggle",
-        f = "Tree Focus",
-    },
-
-    s = {"Open Terminal"},
-
-    r = {"Ruff"},
-
-    c = {
-        name = "Color Schemes",
-        s = "Open"
-    }
-
-}, {prefix = "<leader>"})
+require("which-key").setup {
+  preset = "modern",
+  icons = {
+    mappings = false
+  }
+}
