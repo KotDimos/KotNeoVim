dc9e21a8f7286c09e135c36132d2ebc7215974d0
KotDimos <KotDimos@gmail.com>
2024-10-07 20:21:03 +0300

    feat: add gopls for lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,9 @@
 -- Setup language servers.
 local lspconfig = require('lspconfig')
+
+-- https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim-install
+lspconfig.gopls.setup({})
+
 lspconfig.pyright.setup {
 	settings = {
 	    pyright = {
@@ -14,56 +18,3 @@ lspconfig.pyright.setup {
 		},
 	},	
 }
-
--- Setup Ruff Linter
-lspconfig.ruff_lsp.setup {
-  init_options = {
-    settings = {
-      -- Any extra CLI arguments for `ruff` go here.
-      args = {
-		"--select=E,F,UP,N,I,ASYNC,S,PTH",
-		"--line-length=100",
-		"--respect-gitignore",  -- Исключать из сканирования файлы в .gitignore
-      	"--target-version=py311"
-      },
-    }
-  }
-}
-
--- Global mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
--- vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-
--- Use LspAttach autocommand to only map the following keys
--- after the language server attaches to the current buffer
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'lD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'ld', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'lk', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'i', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    
-    -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    -- vim.keymap.set('n', '<space>wl', function()
-    --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    -- end, opts)
-    
-    vim.keymap.set({ 'n', 'v' }, '<space>r', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<space>f', function()
-      vim.lsp.buf.format { async = true }
-    end, opts)
-  end,
-})
