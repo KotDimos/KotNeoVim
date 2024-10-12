cc2ad2dfe32d5c70d0d2643dec2d8acc64dbc05d
KotDimos <KotDimos@gmail.com>
2024-10-12 07:31:31 +0300

    chore: update pyright

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,17 +4,4 @@ local lspconfig = require('lspconfig')
 -- https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim-install
 lspconfig.gopls.setup({})
 
-lspconfig.pyright.setup {
-	settings = {
-	    pyright = {
-	      -- Using Ruff's import organizer
-	      disableOrganizeImports = true,
-	    },
-	    python = {
-	      analysis = {
-	        -- Ignore all files for analysis to exclusively use Ruff for linting
-	        ignore = { '*' },
-	        },
-		},
-	},	
-}
+lspconfig.pyright.setup({})
