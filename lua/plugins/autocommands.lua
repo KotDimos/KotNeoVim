43f569696b63031ec97a64c8f6e7ae75867ee162
KotDimos <KotDimos@gmail.com>
2024-11-18 21:59:11 +0300

    feat: init autocommand trim whitespaces

---
+++ lua/plugins/autocommands.lua
@@ -0,0 +1,9 @@
+-- trim whitespace
+vim.api.nvim_create_autocmd("BufWritePre", {
+    callback = function()
+        vim.cmd([[
+         keeppatterns %s/\s\+$//e
+      ]])
+    end,
+    group = vim.api.nvim_create_augroup("TrimWhitespace", { clear = true }),
+})
