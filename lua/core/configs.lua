f2ec0dccd189f6af984e5cc84a367588b29a4528
KotDimos <KotDimos@gmail.com>
2025-11-27 20:20:54 +0300

    chore: update comment

--- lua/core/configs.lua
+++ lua/core/configs.lua
@@ -8,11 +8,11 @@ vim.opt.wrap = false
 vim.wo.linebreak = true
 vim.opt.virtualedit = "block"
 vim.opt.undofile = true
-vim.opt.shell = "/bin/zsh"             -- Shell по умолчанию
-vim.opt.swapfile = false               -- Отключить swap файлы nvim
-vim.opt.encoding = "utf-8"             -- Кодировка utf-8
-vim.opt.cursorline = true              -- Выделять активную строку где находится курсор
-vim.opt.cursorcolumn = true            -- Выделять активный столбец где находится курсор
+vim.opt.shell = "/bin/zsh"             -- base shell
+vim.opt.swapfile = false               -- disable swap file nvim
+vim.opt.encoding = "utf-8"             -- encoding utf-8
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
 -- vim.opt.fileformat = "unix"
 
 -- Nvim-Tree
@@ -21,46 +21,46 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 -- Scroll
-vim.opt.so = 30                        -- При скролле курсор всегда по центру
+vim.opt.so = 30                        -- scroll center
 
 -- Search
-vim.opt.ignorecase = true              -- Игнорировать регистр при поиске
-vim.opt.smartcase = true               -- Не игнорирует регистр если в паттерне есть большие буквы
-vim.opt.hlsearch = true                -- Подсвечивает найденный паттерн
-vim.opt.incsearch = true               -- Интерактивный поиск
+vim.opt.ignorecase = true              -- ignore register by search
+vim.opt.smartcase = true               -- not ignore register by search if there are large letter
+vim.opt.hlsearch = true                -- highlights the found pattern
+vim.opt.incsearch = true               -- interactive search
 
 -- Mouse
-vim.opt.mouse = "a"                    -- Возможность использовать мышку
+vim.opt.mouse = "a"
 vim.opt.mousefocus = true
 
--- Совместимость vim с русской клавиатурой
+-- compatibility with Russian keyboard
 vim.opt.langmap="ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz"
 
 -- Line Numbers
-vim.opt.number = true                  -- Показывает номера строк
-vim.opt.relativenumber = true          -- Показывает расстояние к нужной строке относительно нашей позиции
-vim.wo.number = true                   -- Показывает номера строк
-vim.wo.relativenumber = true           -- Показывает расстояние к нужной строке относительно нашей позиции
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.wo.number = true
+vim.wo.relativenumber = true
 
 -- Splits
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 
 -- Clipboard
-vim.opt.clipboard = "unnamedplus"      -- Разрешить общий буфер обмена
+vim.opt.clipboard = "unnamedplus"      -- shared clipboard
 
 -- Shorter messages
 vim.opt.shortmess:append("c")
 
 -- Indent Settings
-vim.opt.expandtab = true               -- Превратить все tab в пробелы
+vim.opt.expandtab = true               -- turn all tabs into spaces
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
-vim.opt.smartindent = true             -- Копировать отступ на новой строке
-vim.opt.cindent = true                 -- Автоотступы
-vim.opt.smarttab = true                -- Tab перед строкой вставит shiftwidht количество табов
-vim.opt.colorcolumn = "100"            -- Количество символов на строке
+vim.opt.smartindent = true             -- copy the indentation on a new line
+vim.opt.cindent = true
+vim.opt.smarttab = true
+vim.opt.colorcolumn = "100"            -- number of characters per line
 
 -- Color
 vim.o.background = "dark"
