c060af166227f06a62ae868962fc041479e13b06
KotDimos <KotDimos@gmail.com>
2025-12-27 17:57:50 +0300

    refactor: update telescope

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -28,7 +28,7 @@ require("lazy").setup({
     { 'hrsh7th/vim-vsnip' },
 
     {
-      'nvim-telescope/telescope.nvim', tag = '0.1.8',
+      'nvim-telescope/telescope.nvim', tag = 'v0.2.0',
       dependencies = { 'nvim-lua/plenary.nvim' }
     },
 
